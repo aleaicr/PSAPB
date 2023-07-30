81d7de23909bbff6fa5a754ae3cc882ce8094f52
Alexis Contreras R <ale.aicr@gmail.com>
2023-07-29 21:09:25 -0400

    Update saveResults.m

--- saveResults.m
+++ saveResults.m
@@ -1,5 +1,14 @@
-function [] = saveResults()
-    % Save results
-    % Inputs    
-    
+function [] = saveResults(fileFolder,fileName,sCond,pq)
+% Author: Alexis Contreras R.
+% Save the obtained serviceability condition from the simulation for
+% the respective pedestrian quantity (pq) in a .txt file
+% 
+% Inputs
+% fileFolder: folder where the file will be saved
+% fileName: name of the file
+% sCond: serviceability condition
+% pq: pedestrian quantity
+%
+% Outputs
+% -
 end
